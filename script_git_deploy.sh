b2537fca794130d31b790a690c286adf68200cd7
Bruno <bruno-sf@users.noreply.github.com>
2020-02-21 21:50:27 -0300

    Update script_git_deploy.sh
    version 3 - ok
    
    Next version do a third option to install the git server cert only for the user like:
    git config --global http."https://git.mycompany.com/".sslCAInfo ~/git-mycompany-com.pem

--- script_git_deploy.sh
+++ script_git_deploy.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 #---------------------------------------------------#
-#	Data de criacao: 02/08/2019	                    #
-#	Ultima Edicao: 02/10/2019	                    #
+#   Data de criacao: 02/08/2019                     #
+#   Ultima Edicao: 21/02/2020                       #
 #---------------------------------------------------#
-#   Versao: 02                                     	#
+#   Versao: 03                                      #
 #   Bugs: brunosilvaferreira@protonmail.com         #
 #---------------------------------------------------#
-#   Descricao: Script para deploy do GIT pessoal.   #
-#	Caso ja exista o repositorio, dara um pull.		#
+#   Descricao: Script para deploy e sync do meu     #
+#   servidor GIT pessoal.                           #
 #---------------------------------------------------#
 
 # [CONFS]
 NOME_SCRIPT="Script GIT Deploy e Sync"
-TOKEN_WEB="XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+TOKEN_WEB="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 PROTO="https"
-PORT="YYYY"
-HOST="XXXXXX" # [Git Gogs Rasp2 Meier]
-GIT_USER="brunof"
+PORT="3000"
+HOST="XXXXXXXXXXXXXXXXXXXXXXXXXXXx" # [Git Gogs Rasp2 Meier]
+GIT_USER="XXXXXXXXXXXXXX"
+CA_CRT_PATH="/etc/ssl/certs/ca-certificates.crt"
+CRT="-----BEGIN CERTIFICATE-----
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+PUT THE CONTENT OF YOUR GIT SRV CERTIFICATE HERE ;)
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+-----END CERTIFICATE-----"
 
 # [REPOS]
 DIR_LOCAL_REPO="$HOME/git_pessoal/"
@@ -31,6 +44,32 @@ GIT_AWS_REPO="aws"
 REPOS="$GIT_AWS_REPO $GIT_CONFS_REPO $GIT_DOCKER_REPO $GIT_PYTHON_REPO $GIT_QUBES_REPO $GIT_SCRIPTS_REPO"
 
 # [FUNCTIONS]
+    #Por questao de seguranca prefiro verificar o cert TLS do meu server o cert hardcoded no script.
+    #For security measure never disable ssl on git and please make sure every time the TLS cert is the one you trust.
+fn_chk_crt () {
+    #Extract and Check if the remote TLS Cert matches with the hard coded one in this script ;)
+    REMOTE_CRT=$(echo -n | openssl s_client -showcerts -connect $HOST:$PORT \
+          2>/dev/null  | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p')
+    [ -z "$REMOTE_CRT" ] && { echo "Can't extract TLS certificate from host...
+        Aborting the security checks..."; return; }
+
+    [ "$REMOTE_CRT" != "$CRT" ] && { echo "[Security alert]: The certificate from host don't look correct. If you change it recently, please update CRT var on this script, or you can be suffering a phishing attack and should not continue!"; exit 1; }
+    [ -f "$CA_CRT_PATH" ] && { echo -n "$CRT" | sed '/-----/d' | grep -qxwf "$CA_CRT_PATH" && return; } #Cert already "installed"
+    echo "Can't find the content of the host cert on your local CA file ("$CA_CRT_PATH")."
+    read -p "Do you wish to install it (y/n)?" choice
+    case "$choice" in
+    y|Y ) fn_install_crt;;
+    n|N ) echo "Ok, I will NOT install the cert as you wish...";;
+    * ) echo "Invalid answer, doing nothing..."; return;;
+    esac
+}
+fn_install_crt () {
+    #Install the cert appending it on CA File
+    [ -w "$CA_CRT_PATH" ] && echo "$CRT" >> $CA_CRT_PATH && echo "[OK]: Cert successfully installed!" && return 0
+    echo "[FAIL]: Sorry, the user ["$USER"] don't have permission to write on ["$CA_CRT_PATH"] and so do I.Ok, last shot...Trying with sudo..."
+    echo "$CRT" | sudo tee -a $CA_CRT_PATH && echo "[OK]: Cert successfully installed!" && return 0
+}
+
 fn_clone_repo () {
     # Call this func only if $DIR_LOCAL_REPO don't exist!
     while [ $# -gt 0 ] ; do
@@ -44,29 +83,27 @@ fn_clone_repo () {
 
 
 fn_pull_repo () {
-	while [ $# -gt 0 ] ; do
-    	[ -z "$1" ]  && break;
+    while [ $# -gt 0 ] ; do
+        [ -z "$1" ]  && break;
         CURRENT_REPO=$DIR_LOCAL_REPO$1
-        
+
         [ -d "$CURRENT_REPO" ] \
             || { echo "[ERROR]: Directory don't exist, please review it: $CURRENT_REPO "; exit 1; }
-    	
+
         git -C "$CURRENT_REPO" status \
             || { echo "[ERROR]: Not a regular git repository, please review it: $CURRENT_REPO"; exit 1; }
-        
+
         git -C "$CURRENT_REPO" pull \
             || { echo "[ERROR]: Can't pull this repository: $CURRENT_REPO"; exit 1; }
         shift
-	done
+    done
 }
 
 # [BEGIN]
-#Check local REPO
-git config --global http.sslverify false
+# Check local REPO
 if [ -d  "$DIR_LOCAL_REPO" ]; then
     fn_pull_repo $REPOS
 else
     mkdir -p "$DIR_LOCAL_REPO" || { echo "[ERROR]: Can't create dir: $DIR_LOCAL_REPO"; exit 1; }
-    fn_clone_repo $REPOS
+    fn_chk_crt && fn_clone_repo $REPOS
 fi
-git config --global http.sslverify true
