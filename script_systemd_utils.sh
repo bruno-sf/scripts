469f0d5e887a4e650b14a8e80a544c48f54dcb28
Bruno <bruno-sf@users.noreply.github.com>
2020-06-22 21:16:42 -0300

    Update script_systemd_utils.sh
    more funcs ;)

--- script_systemd_utils.sh
+++ script_systemd_utils.sh
@@ -39,7 +39,7 @@ fn_echo_color () {
     case $COLOR in
     "RED") echo -e "\e[31;1m $1 \e[m" ;;
     "GREEN") echo -e "\e[32;1m $1 \e[m" ;;
-    "BLUE") echo -e "\e[34;1m $1 \e[m" ;;
+    "BLUE") echo -e "\e[34;1m$1 \e[m" ;;
     *) echo -e "\e[33;1m $1 \e[m" ;;
     esac
 }
@@ -60,14 +60,18 @@ fn_menu () {
     echo "12)Show datetime system info"
     echo "13)Show today only logs"
     echo "14)Show logs since yesterday"
+    echo "15)Check each service time consumption at startup"
+    echo "16)Check system boot time"
+    echo "17)Show system status"
+    echo "18)Kill all processes associated with a service"
     fn_echo_color "0)Exit" "BLUE"
-    echo "Type the number option:"
+    echo "Type the number of option:"
     read OPT
 
     #Verifica se é válido 
     if [ -z $OPT ]
     then
-        echo="Invalid option..."
+        echo "Invalid option..."
     else
 
         case $OPT in
@@ -85,8 +89,11 @@ fn_menu () {
         12) fn_timedate ;;
         13) fn_tod_logs ;;
         14) fn_yest_logs ;;
-
-
+        15) fn_chk_svc_time ;;
+        16) fn_chk_boot_time ;;
+        17) fn_sys_status ;;
+        18) fn_kill_procs_svc ;;
+        
         0) { echo "See you!"; exit 0; } ;;
         *) echo "Invalid option!" ;;
         esac
@@ -94,32 +101,32 @@ fn_menu () {
 }
 
 fn_lst_unit_files () {
-    #[1]A unit file is a plain text ini-style file that encodes information about a service, a socket, a device, a mount point, an automount point, a swap file or partition, a start-up target, a watched file system path, a timer controlled and supervised by systemd, a resource management slice, or a group of externally created processes.
+    # [1] A unit file is a plain text ini-style file that encodes information about a service, a socket, a device, a mount point, an automount point, a swap file or partition, a start-up target, a watched file system path, a timer controlled and supervised by systemd, a resource management slice, or a group of externally created processes.
     systemctl list-unit-files || return 1    
 }
 
 fn_lst_unit_files_grep () {
-    #[2]Same as fn_lst_unit_files but with grep duh.
+    # [2] Same as fn_lst_unit_files but with grep duh.
     echo "Type the name of the service:"
     read SVC_NAME
     systemctl list-unit-files | grep -i "$SVC_NAME"
 }
 
 fn_lst_units () {
-    #[3]Listing active units displays a lot of useful information about your loaded and active services.
+    #[3] Listing active units displays a lot of useful information about your loaded and active services.
     systemctl list-units || return 1
     return 0
 }
 
 fn_lst_units_grep () {
-    #[4]Same as fn_lst_units but with grep duh.
+    # [4] Same as fn_lst_units but with grep duh.
     echo "Type the name of the service:"
     read SVC_NAME
     systemctl list-units | grep -i "$SVC_NAME"
 }
 
 fn_start_svc () {
-    #[5] starting, stopping, or restarting a service. and  check the status of a service.
+    # [5] starting, stopping, or restarting a service. and  check the status of a service.
     echo "Type the name of the service you wish to START:"
     read SVC_NAME
     systemctl start $SVC_NAME && systemctl status "$SVC_NAME"
@@ -127,14 +134,14 @@ fn_start_svc () {
 }
 
 fn_stop_svc () {
-    #[6] starting, stopping, or restarting a service. and  check the status of a service.
+    # [6] starting, stopping, or restarting a service. and  check the status of a service.
     echo "Type the name of the service you wish to STOP:"
     read SVC_NAME
     systemctl stop $SVC_NAME && systemctl status $SVC_NAME
 }
 
 fn_rst_svc () {
-    #[7] starting, stopping, or restarting a service. and  check the status of a service.
+    # [7] starting, stopping, or restarting a service. and  check the status of a service.
     echo "Type the name of the service you wish to RESTART:"
     read SVC_NAME
     systemctl restart $SVC_NAME && systemctl status $SVC_NAME
@@ -167,24 +174,50 @@ fn_chk_boot () {
 }
 
 fn_host_info () {
-    # [11]Show host info
+    # [11] Show host info
     hostnamectl
 }
 
 fn_timedate () {
-    # [12]Show datetime system info
+    # [12] Show datetime system info
     timedatectl
 }
 
 fn_tod_logs () {
-    # [13]Show today logs
+    # [13] Show today logs
     journalctl --since="today"
 }
 
 fn_yest_logs () {
-    # [14]Show yesterday logs
+    # [14] Show yesterday logs
     journalctl --since="yesterday"
 }
+
+fn_chk_svc_time () {
+    # [15] Check each service time consumption at startup
+    systemd-analyze blame
+}
+
+fn_chk_boot () {
+    # [16] Check system boot time
+    systemd-analyze
+}
+
+fn_sys_status () {
+    # [17] Show system status
+    systemctl status
+}
+
+fn_kill_procs_svc () {
+    # [18] Kill all processes associated with a service
+    echo "Type the name of the service you wish to KILL all processes associated with:"
+    read SVC_NAME
+    systemctl kill "$SVC_NAME"
+}
+
 clear
 fn_banner
-fn_menu
+while true
+do
+    fn_menu
+done
